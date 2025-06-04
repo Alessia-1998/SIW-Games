28bf2ff2fccc436fbfe4bef6c7907723878142d4
danieleene <91674916+danieleene@users.noreply.github.com>
2025-06-04 09:25:55 +0200

    Aggiornamento in VideogiocoRepository.java
    Aggiunti i metodi, dentro il repository, per le ricerche rispettivamente per titolo(anche parziale) e per anno

--- src/main/java/it/uniroma3/siw/repository/VideogiocoRepository.java
+++ src/main/java/it/uniroma3/siw/repository/VideogiocoRepository.java
@@ -6,4 +6,7 @@ import it.uniroma3.siw.model.Videogioco;
 
 public interface VideogiocoRepository extends CrudRepository<Videogioco, Long> {
 
+  List<Videogioco> findByTitoloContainingIgnoreCase(String titolo);
+  List<Videogioco> findByAnno(int anno);
+
 }
