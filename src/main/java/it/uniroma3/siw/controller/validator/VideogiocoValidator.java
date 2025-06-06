225856a304db519a2d269b83c0991ac1d8df7c5c
Alessia-1998 <alessiacaristo98@gmail.com>
2025-06-06 14:54:01 +0200

    Aggiunto VideogiocoValidator.java VUOTO
    Per il  collaboratore

---
+++ src/main/java/it/uniroma3/siw/controller/validator/VideogiocoValidator.java
@@ -0,0 +1,9 @@
+package it.uniroma3.siw.controller.validator;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Validator;
+
+@Component
+public class VideogiocoValidator implements Validator {
+
+}
